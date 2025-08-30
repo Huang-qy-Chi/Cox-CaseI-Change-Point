afa78404584085422038c061911eb17245312806
Huang-qy-Chi <79854798+Huang-qy-Chi@users.noreply.github.com>
2025-08-30 16:47:51 +0800

    Update mn_boot.py

--- Python_version/mn_boot.py
+++ Python_version/mn_boot.py
@@ -136,6 +136,57 @@ def interval_zeta(data,m1,zeta,m=3,B=5000,seq=0.01,seed=42,alpha=0.05):
     }
 
 
+# For parallel calculation: bootstrap
+def interval_zeta_par(data,m1,zeta,m=3,B=1000,seq=0.01,seed=42,alpha=0.05,n_jobs=None):
+    Z_2 = data['Z_2']
+    n = len(Z_2)
+
+    # Bootstrap data
+    data_boot = bootstrap_dict_mixed(data,n_boot=m1, n_samples=B, random_state=seed)
+    # Bootstrap zeta estimation: B times
+    # zeta_m = []
+    # for l in range(B):   #replication 
+    #     set_seed(seed+l)
+    #     # sampled_keys = np.random.choice(keys, size=len(keys), replace=True)
+    #     # data_boot = {i: data[key] for i, key in enumerate(sampled_keys)}
+    #     # data_boot1 = rename_dict_keys(data_boot, key_mapping)
+    #     data_boot1 = data_boot[l]
+    #     Res_m = cpcph(data_boot1,m=m,B=B,seq=seq)
+    #     zeta_m.append(Res_m['zeta'])
+
+    def parallel_boot(data_boot, key_to_keep='zeta', n_jobs= None):
+        """
+        中文注释没空改了>O<
+        data_boot: 包含 B 个重抽样数据集的字典
+        n_jobs: 并行进程数（None 表示使用所有可用 CPU 核心）
+        返回: 所有回归系数的 NumPy 数组
+        """
+        if n_jobs is None:
+            n_jobs = min(cpu_count() - 1, 60, len(data_boot))   # 
+        
+        # 获取所有数据集
+        datasets = [data_boot[i] for i in range(len(data_boot))]
+        
+        # 使用 multiprocessing.Pool 并行计算
+        # Use "partial" to pass fixed parameters
+        sim_func = partial(cpcph, m=m, B=B, seq=seq)
+        with Pool(processes=n_jobs) as pool:
+            results = pool.map(sim_func, datasets)
+
+        return np.array([result[key_to_keep] for result in results])
+    
+    zeta_m = parallel_boot(data_boot, key_to_keep='zeta', n_jobs=n_jobs)
+    diff_zeta = np.abs(zeta_m-zeta)
+    diff_zeta = np.sort(diff_zeta)
+    quantile_alpha = int(np.floor(B*(1-alpha/2))-1)
+    inter_length = diff_zeta[quantile_alpha]
+    interval = np.array([zeta-m1*inter_length/n, zeta+m1*inter_length/n],dtype='float32')
+
+
+    return{
+        'inter_length': inter_length,
+        'interval': interval
+    }
 
 
 
